1501bce9cf67127d1b3647e49dd0d868a926d8ec
Carlos <ocariocawebdesign@gmail.com>
2021-11-19 08:55:14 -0300

    Instalação do plugin Reat Helmet e colocação de metagtags

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,40 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+
+    `gatsby-plugin-react-helmet`,
+    
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "O Carioca Web",
+        short_name: "O Carioca Web",
+        background_color: "#fff",
+        theme_color: `#440055`,
+        // Setting a color is optional.
+        display: "standalone",
+        icon: "src/img/monster.png",
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
+
+
+    
+
+
+
+
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,54 @@
-import React from "react";
-import Header from "../components/Header";
-import Content from "../components/Content";
-import Footer from "../components/Footer";
-import "./css/style.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react"
+import Header from "../components/Header"
+import Content from "../components/Content"
+import Footer from "../components/Footer"
+import "./css/style.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Helmet } from "react-helmet"
+
+class Application extends React.Component {
+  render() {
+    return (
+      <div className="application">
+        <Helmet>
+          <meta charSet="utf-8" />
+
+          <meta name="title" content="Você ainda não tem um site?" />
+          <meta
+            name="description"
+            content="Dê vida a seu negócio e exista na internet! Qual maior site de buscas? Não precisamos nem pensar. "
+          />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://ocariocaweb.gatsbyjs.io/" />
+          <meta property="og:title" content="Você ainda não tem um site?" />
+          <meta
+            property="og:description"
+            content="Dê vida a seu negócio e exista na internet! Qual maior site de buscas? Não precisamos nem pensar."
+          />
+          <meta property="og:image" content="" />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta
+            property="twitter:url"
+            content="https://ocariocaweb.gatsbyjs.io"
+          />
+          <meta
+            property="twitter:title"
+            content="Você ainda não tem um site?"
+          />
+          <meta
+            property="twitter:description"
+            content="Dê vida a seu negócio e exista na internet! Qual maior site de buscas? "
+          />
+
+          <title>O Carioca Web Desing</title>
+          <link rel="canonical" href="https://ocariocaweb.gatsbyjs.io/" />
+        </Helmet>
+      </div>
+    )
+  }
+}
 
 const Home = () => (
   <>
