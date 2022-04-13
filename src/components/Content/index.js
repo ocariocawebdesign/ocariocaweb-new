abef651e7b8a119a69f75a31005c4f522ddc80f1
Carlos Abreu <ocariocawebdesign@gmail.com>
2022-04-13 07:39:48 -0300

    Alteração no telefone
    Alteração no telefone no número

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -195,7 +195,7 @@ const Content = () => (
 
     <div className="centralizar-coffe">
       <a
-        href="https://api.whatsapp.com/send?phone=5521971025148&text=Ol%C3%A1!%20Quero%20informa%C3%A7%C3%B5es%20sobre%20Desenvolvimento%20de%20sites..."
+        href="https://api.whatsapp.com/send?phone=5521967530653&text=Ol%C3%A1!%20Quero%20informa%C3%A7%C3%B5es%20sobre%20Desenvolvimento%20de%20sites..."
         target="_blank"
         rel="noreferrer"
         cacak7
